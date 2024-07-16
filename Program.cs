8304a14bf208f47e4e60217ed0d6337febd2fc4e
Cristiano Groberio <cristiano@rgsystem.com.br>
2024-07-16 09:24:11 -0300

    Inicial

--- Program.cs
+++ Program.cs
@@ -2,13 +2,16 @@ using rgInfra.Components;
 using Blazorise;
 using Blazorise.Bootstrap5;
 using Blazorise.Icons.FontAwesome;
+using static rgInfra.ViewModel.Usuario.LoginApiViewModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddRazorComponents();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
 builder.Services
     .AddBlazorise(options =>
     {
@@ -31,6 +34,23 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.UseRouting();
+app.MapBlazorHub();
+//app.MapFallbackToPage("/_Host");
+app.MapPost("/api/login", async (RequestLogin loginModel) =>
+{
+    if (loginModel.login == "cristiano@rgsystem.com.br" && loginModel.senha == "Cris1605@")
+    {
+        return Results.Ok(new ResponseLogin
+        {
+            accessToken = "dummyAccessToken",
+            refreshToken = "dummyRefreshToken",
+            description = "Login successful"
+        });
+    }
+    return Results.Unauthorized();
+});
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
---
+++ ViewModel/Usuario/DadosUsuarioViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace rgInfra.ViewModel.Usuario
+{
+	public class DadosUsuarioViewModel
+	{
+		public class RequestDadosUsuario
+		{
+			public string email { get; set; }
+		}
+
+		public class ResponseDadosUsuario
+		{
+			public string id { get; set; }
+			public string userName { get; set; }
+			public string normalizedUserName { get; set; }
+			public string email { get; set; }
+			public string normalizedEmail { get; set; }
+			public bool emailConfirmed { get; set; }
+			public string passwordHash { get; set; }
+			public string securityStamp { get; set; }
+			public string concurrencyStamp { get; set; }
+			public string phoneNumber { get; set; }
+			public bool phoneNumberConfirmed { get; set; }
+			public bool twoFactorEnabled { get; set; }
+			public DateTime lockoutEnd { get; set; }
+			public bool lockoutEnabled { get; set; }
+			public int accessFailedCount { get; set; }
+			public string fullName { get; set; }
+			public DateTime birthDate { get; set; }
+			public bool isLoginWithCertificate { get; set; }
+			public string cpf { get; set; }
+		}
+	}
+}
